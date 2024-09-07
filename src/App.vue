8be99ae50e676225ac8c856970768e3739b12830
kun <2385674484@qq.com>
2024-09-07 10:50:06 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-console.log(asd)
+console.log(123)
 </script>
 
 <template>
