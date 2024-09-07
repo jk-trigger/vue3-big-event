c6e77b70b5c9c093a475435ff3d056d4120a290a
kun <2385674484@qq.com>
2024-09-07 09:52:40 +0800

    有错误

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-// console.log(asd)
+console.log(asd)
 </script>
 
 <template>
