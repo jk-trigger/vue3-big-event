a0a3693ebd49451c619b1f0f8a7529fdb189fcbf
kun <2385674484@qq.com>
2024-09-07 10:51:53 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-console.log(123)
+console.log(12312312)
 </script>
 
 <template>
